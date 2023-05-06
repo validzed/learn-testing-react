386181e5ed54db7f42298e010c0ae07bbcf2fff0
validzed <validtry@gmail.com>
2023-05-06 19:27:32 +0700

    Add test detectTriangle full

--- detectTriangle.js
+++ detectTriangle.js
@@ -10,6 +10,16 @@ function detectTriangle(a, b, c) {
   if (a + b <= c) {
     throw new Error('Input is triangle inequality violation');
   }
+
+  if (a === b && b === c) {
+    return 'Segitiga Sama Sisi';
+  }
+
+  if (a === b || b === c) {
+    return 'Segitiga Sama Kaki';
+  }
+
+  return 'Segitiga Sembarang';
 }
 
 module.exports = detectTriangle;
--- detectTriangle.test.js
+++ detectTriangle.test.js
@@ -30,5 +30,24 @@ describe('detectTriangle function', () => {
     expect(() => detectTriangle(1, 2, 3)).toThrowError('Input is triangle inequality violation');
     expect(() => detectTriangle(5, 1, 3)).toThrowError('Input is triangle inequality violation');
     expect(() => detectTriangle(3, 4, 1)).toThrowError('Input is triangle inequality violation');
+    expect(() => detectTriangle(5, 2, 1)).toThrowError('Input is triangle inequality violation');
+  });
+
+  it('should return "segitiga sama sisi" when all input are equal', () => {
+    expect(detectTriangle(1, 1, 1)).toBe('Segitiga Sama Sisi');
+    expect(detectTriangle(2, 2, 2)).toBe('Segitiga Sama Sisi');
+    expect(detectTriangle(3, 3, 3)).toBe('Segitiga Sama Sisi');
+  });
+
+  it('should return "segitiga sama kaki" when two input are equal', () => {
+    expect(detectTriangle(2, 2, 3)).toBe('Segitiga Sama Kaki');
+    expect(detectTriangle(5, 6, 5)).toBe('Segitiga Sama Kaki');
+    expect(detectTriangle(7, 8, 8)).toBe('Segitiga Sama Kaki');
+  });
+
+  it('should return "segitiga sembarang" when all input are different', () => {
+    expect(detectTriangle(2, 3, 4)).toBe('Segitiga Sembarang');
+    expect(detectTriangle(5, 6, 7)).toBe('Segitiga Sembarang');
+    expect(detectTriangle(8, 9, 10)).toBe('Segitiga Sembarang');
   });
 });
